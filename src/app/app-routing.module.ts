c51f91784cec51b0479f7633ea8cde303fff0e72
SainuEsrever <95266744+SainuEsrever@users.noreply.github.com>
2023-12-17 23:04:13 +0700

    app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+import {HomeComponent} from './components/home.component';
+
+
+const routes : Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  {path : 'home', component: HomeComponent},
+]
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
