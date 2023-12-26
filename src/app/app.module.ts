1af2142b6ab136e0fc138832b10fd27a200a416d
SainuEsrever <95266744+SainuEsrever@users.noreply.github.com>
2023-12-26 20:51:39 +0700

    repair conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- Import FormsModule
 import { HttpClientModule } from '@angular/common/http'; // <-- Import HttpModule
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
