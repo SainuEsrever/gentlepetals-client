c1c0fc409ee5b8aeb5065f41a9323fd6db0ddcf4
Muy111 <mypt21406@st.uel.edu.vn>
2023-12-27 21:02:27 +0700

    Navbar and footer update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { ElementRef, HostListener } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  constructor(private el: ElementRef) {}
 
-}
+  @HostListener('window:scroll', ['$event'])
+  handleScroll() {
+    const navbar: HTMLElement = this.el.nativeElement.querySelector('.navbar');
+    window.scrollY >= 50 ? navbar.classList.add('sticky') : navbar.classList.remove('sticky');
+}}
