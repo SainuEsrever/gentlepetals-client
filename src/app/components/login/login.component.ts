c280fbc03ceab9a19268448bc26b45c109ad231c
huentk21406 <95629029+huentk21406@users.noreply.github.com>
2023-12-27 14:06:30 +0700

    fix valid T.T

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
-
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AccountService } from '../_sevices/account.service';
-import { AlertService } from '../_sevices/alert.service';
-import { first } from 'rxjs';
-import { User } from '../_models/user';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -21,8 +16,7 @@ export class LoginComponent implements OnInit{
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    private accountService: AccountService,
-    private alertService: AlertService
+
 ) { }
     // loginModel = new User("",'','','','')
   ngOnInit(){
@@ -32,33 +26,8 @@ export class LoginComponent implements OnInit{
   });
   }
   
-  get email() { return this.form.controls['emai'] }
-  get password() { return this.form.controls['password'] }
-
-    onSubmit() {
-        this.submitted = true;
-
-        // reset alerts on submit
-        this.alertService.clear();
-
-        // stop here if form is invalid
-        if (this.form.invalid) {
-            return;
-        }
-        
-        // this.loading = true;
-        // this.accountService.login(this.email.value,this.password.value)
-        //     .pipe(first())
-        //     .subscribe({
-        //         next: () => {
-        //             // get return url from query parameters or default to home page
-        //             const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-        //             this.router.navigateByUrl(returnUrl);
-        //         },
-        //         error: error => {
-        //             this.alertService.error(error);
-        //             this.loading = false;
-        //         }
-        //     });
-    }
+  get email() 
+  { return this.form.controls['email']; }
+  get password() 
+  { return this.form.controls['password']; }
 }
