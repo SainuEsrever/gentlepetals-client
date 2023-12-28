6ce6ccbd7eb2815a76e745f7815876c670270caf
mtuan223 <95267092+mtuan223@users.noreply.github.com>
2023-12-28 14:59:11 +0700

    validation changepw

--- src/app/components/dashboard-change-password/dashboard-change-password.component.ts
+++ src/app/components/dashboard-change-password/dashboard-change-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-change-password',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard-change-password.component.css']
 })
 export class DashboardChangePasswordComponent {
+  changePasswordForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.changePasswordForm = this.fb.group(
+      {
+        currentPassword: ['', Validators.required],
+        newPassword: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+    passwordMatchValidator(control: FormGroup) {
+      const newPassword = control.get('newPassword')?.value;
+      const confirmPassword = control.get('confirmPassword')?.value;
+  
+      return newPassword === confirmPassword ? null : { passwordMismatch: true };
+    }
+    onSubmit() {
+      if (this.changePasswordForm.valid) {
+        console.log('Form submitted:', this.changePasswordForm.value);
+      }
+    }
 }
